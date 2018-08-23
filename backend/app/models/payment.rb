c0ca94aa8b97845d260a95ff8153c9c24f130429
Jose Salcido <jose@otfusion.org>
2018-08-22 23:44:53 -0700

    catch correctly exceptions for ActiveModel

--- backend/app/models/payment.rb
+++ backend/app/models/payment.rb
@@ -1,5 +1,9 @@
 class Payment < ApplicationRecord
 
   validates_presence_of :date, :quantity, :concept
+  validates :quantity, numericality: {
+    less_than_or_equal_to: 9_223_372_036_854_775_807,
+    only_integer: true
+  }
 
 end
