cd2439692eaaf198979e50111d20744e57ae1626
Jose Salcido <jose@otfusion.org>
2018-08-22 19:28:07 -0700

    use port 8080 by default and CORS from localhost:3000 and return username in JWT

--- backend/app/auth/authenticate_user.rb
+++ backend/app/auth/authenticate_user.rb
@@ -6,7 +6,7 @@ class AuthenticateUser
   end
 
   def call
-    JsonWebToken.encode(user_id: user.id) if user
+    JsonWebToken.encode(user_id: user.id, username: user.name) if user
   end
 
   private
